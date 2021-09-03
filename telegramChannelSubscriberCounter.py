4f4a4ef60c3d4022cbaef4c1d22c106643d2b5d7
Kama <54350573+KElee01@users.noreply.github.com>
2021-09-03 14:04:15 +0800

    Add files via upload

---
+++ telegramChannelSubscriberCounter.py
@@ -0,0 +1,29 @@
+# coding = utf-8
+from selenium import webdriver
+import time
+
+dr = webdriver.Safari()  # open safari
+
+print('打开网站')
+url = 'https://t.me/NewlearnerChannel'
+dr.get(url)
+
+while True:
+    try:
+        subscriber_line = dr.find_element_by_xpath('/html/body/div[1]/div[2]/div[3]').text
+        break
+    except:
+        pass
+subscriber_count = ""
+for letter in subscriber_line:
+    try:
+        int(letter)
+        subscriber_count += letter
+    except:
+        pass
+write_in = "{%s, %s}\n"% (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), subscriber_count)
+print(write_in)
+log = open('./telegramSubscriberCountLog', 'a+')
+log.write(write_in)
+log.close()
+dr.quit()
